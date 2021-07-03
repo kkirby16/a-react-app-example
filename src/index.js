5d94d487de4f844a5231039035bd7d25824c0b1b
kkirby16 <kkirby16@my.whitworth.edu>
2021-07-02 18:01:35 -0700

    commit tree 1bc23eda6be21a359d8d797b2d450f6c9764e066

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root")); ///how we render what we want to this location.
+//webpack connects react components to index.html
+//webpack dependecy automatically makes the connection to the index.html
